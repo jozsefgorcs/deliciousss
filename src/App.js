5c080833cea5ed14ddba47b569bcaf00e045f1f1
jozsefgorcs <joeygeek@gmail.com>
2022-10-28 20:43:52 +0200

    Add initial page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Pages from "./pages/Pages";
+import Category from "./components/Category";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Category />
+      <Pages></Pages>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Pages.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Home from "./Home";
+function Pages() {
+  return (
+    <div>
+      <Home></Home>
+    </div>
+  );
+}
+//https://www.youtube.com/watch?v=xc4uOzlndAk&t=2119
+export default Pages;
