49b0f259b0d1fbcad80be7323fe929bef661382b
jozsefgorcs <joeygeek@gmail.com>
2022-10-29 05:47:30 +0200

    add param parsing

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -2,9 +2,24 @@ import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 import { Link, useParams } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function Cuisine() {
-  return <div>Cuisine</div>;
+  const [cuisine, setCuisine] = useState([]);
+  let params = useParams();
+  const getCuisine = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
+    );
+    const recipes = await data.json();
+    setCuisine(recipes.results);
+  };
+
+  useEffect(() => {
+    getCuisine(params.type);
+  }, [params.type]);
+  return <div></div>;
 }
 
 export default Cuisine;
